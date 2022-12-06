25913f0b5ed435ef836ab8379bb5c8ed00679328
Samuel Mohr (MUNI) <mohr@fi.muni.cz>
2022-12-06 13:09:25 +0100

    Day 06

---
+++ day06/README.md
@@ -0,0 +1,4 @@
+# Calorie Counting
+
+Compile this code by ```gcc startmessage.c -o run1``` for the first exercise. Then **echo** the datastream to the program:
+```cat input | ./run1``` 
---
+++ day06/startsequence.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const int token_size = 4;
+
+int main()
+{
+    char *buffer = NULL;
+    size_t characters;
+
+    getline(&buffer,&characters,stdin);
+    
+    char *begin = buffer, *active = buffer + token_size-1, *end = buffer + characters;
+    
+    while(active < end) 
+    {
+        for(char* test = active;test>=active - (token_size-2);test--) 
+          for(char* against = test-1;against>=active-(token_size-1);against--)
+          {
+//          printf("test = %zu , against = %zu, values = %d , %d \n",test,against,*test,*against);
+            if(*test == *against)
+            {
+              active = against+token_size;
+              goto cnt;
+            }
+          }
+        
+        active++;
+        break;
+      cnt:;
+    }
+    if(active > end)
+        active = end;
+    
+    printf("%zu\n", active-begin);
+    
+    free(buffer);
+    exit(0);
+}
